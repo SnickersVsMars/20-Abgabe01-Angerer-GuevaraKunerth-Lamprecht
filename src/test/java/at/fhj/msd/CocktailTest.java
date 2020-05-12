e546c7529e776ea4a5a782ad12c0ec2615d4c581
Samuel Angerer <samuelangerer@Samuels-MacBook-Pro.local>
2020-05-12 15:19:31 +0200

    Updated CocktailTest.java with JavaDoc

--- src/test/java/at/fhj/msd/CocktailTest.java
+++ src/test/java/at/fhj/msd/CocktailTest.java
@@ -5,19 +5,35 @@ import at.fhj.msd.exceptions.BlackoutException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
 import java.util.ArrayList;
 
+/**
+ * Testing class for the class cocktail
+ *
+ * @author Samuel Angerer
+ * @version %I%, %G%
+ * @since 1.1
+ */
 
 @DisplayName("Testing Cocktail implementation")
 class CocktailTest {
+    /**
+     * 3 lists that contain the liquids for the cocktails
+     */
     private ArrayList<Liquid> m1, m2, m3;
+    /**
+     * 3 cocktails to be initialized
+     */
     private Cocktail c1, c2, c3;
+    /**
+     * special liquid that is used more than once, hence declared here
+     */
     private Liquid weingeist;
 
     /**
      * Inits a cocktails object that is non-alcoholic, a cocktails object that is alcoholic and slighltly
-     * under 80% alcohol percentage and one that is above
+     * under 80% alcohol percentage and one that is above. Additionally inits 3 lists that contain the
+     * liquids for the cocktails
      */
     @BeforeEach
     void setup() {
@@ -53,6 +69,10 @@ class CocktailTest {
         }
 
     }
+
+    /**
+     * Test for the constructor, tests if cocktails objects aare created with above values
+     */
     @Test
     @DisplayName("Testing constructor")
     public void testConstructor(){
@@ -61,34 +81,60 @@ class CocktailTest {
         assertEquals("Mild Custom Headache", c2.getName());
         assertEquals(m2, c2.getMixture());
     }
+
+    /**
+     * Tests if the constructor throws the BlackoutException if cocktail to be created is above 80%
+     */
     @Test
     @DisplayName("Testing if constructor throws")
     public void testConstructorThrows(){
         assertThrows(BlackoutException.class, ()->{c3 = new Cocktail("Why would you drink this?", m3);});
     }
+
+    /**
+     * Tests if the volume is properly returned
+     */
     @Test
     @DisplayName("Testing volume getter")
     public void testVolumeGetter(){
         assertEquals(0.5, c1.getVolume(), 0.001);
         assertEquals(0.20, c2.getVolume(), 0.001);
     }
+
+    /**
+     * Tests if the aldohol percent is properly returned
+     */
     @Test
     @DisplayName("Testing alcohol percent getter")
     public void testAlcoholPercentGetter(){
         assertEquals(0.0, c1.getAlcoholPercent(), 0.001);
         assertEquals(77.5, c2.getAlcoholPercent(), 0.001);
     }
+
+    /**
+     * Tests if isAlcoholic returns the proper boolean
+     */
     @Test
     @DisplayName("Testing isAlcoholic")
     public void testIsAlcoholic(){
         assertFalse(c1.isAlcoholic());
         assertTrue(c2.isAlcoholic());
     }
+
+    /**
+     * Tests of addLiquid throws the BlackoutException properly when the liquid to be added would
+     * exceed 80%
+     */
     @Test
     @DisplayName("Testing if addLiquid throws")
     public void testAddLiquidThrows(){
         assertThrows(BlackoutException.class, ()->{c2.addLiquid(weingeist);});
     }
+
+    /**
+     * Tests if the liquid in addLiquid is added properly by comparing the volume of the cocktail before
+     * and after adding
+     */
     @Test
     @DisplayName("Testing addLiquid")
     public void testAddLiquid(){
